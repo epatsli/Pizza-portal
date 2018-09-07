7b908bce2b5378c494f773c10b257c0402b628ca
epatsli <patrycja.a.sliwinska@capgemini.com>
2018-09-07 23:20:08 +0200

    Delete dishes

--- src/app/shopping-cart/shopping-cart.service.ts
+++ src/app/shopping-cart/shopping-cart.service.ts
@@ -42,33 +42,27 @@ export class ShoppingCartService {
         repeat = true;
         this.dishes[i].count += 1; }
       }
-      console.log(repeat);
-      console.log('dd2 ' + this.dishes.length);
     }
     if (repeat === false) {
       dish.count = 1;
       this.dishes.push(dish);
-      console.log(repeat);
     }
- //   this.dishes.push(dish);
- //   const num = dish.price;
-    console.log(dish.id);
   }
 
   delete(dish: Dishes) {
     let i;
-    for (i = 0; i < this.dishes.length; i++) {
-      if (this.dishes[i] === dish) {
-        this.dishes.splice(i, 1);
-        const num = dish.price;
-        this.price = parseFloat((this.price - num * 100 / 100).toFixed(2));
-        break;
+    if (dish.count > 1) {
+      dish.count -= 1;
+    } else if (dish.count === 1) {
+      for (i = 0; i < this.dishes.length; i++) {
+        if (this.dishes[i] === dish) {
+          this.dishes.splice(i, 1);
+          const num = dish.price;
+          this.price = parseFloat((this.price - num * 100 / 100).toFixed(2));
+          break;
+        }
       }
     }
-  }
-
-  removeProduct(id: number) {
-    this.dishes = this.dishes.filter((_item) =>  _item.id !== id );
   }
 }
 
